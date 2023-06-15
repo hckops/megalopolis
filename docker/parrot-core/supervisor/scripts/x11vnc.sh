d312fe2b77d4cc32971696cfc5e5e362dccf3a18
niqdev <niqdev@users.noreply.github.com>
2023-06-15 22:19:07 +0100

    update parrot-core

--- docker/parrot-core/supervisor/scripts/x11vnc.sh
+++ docker/parrot-core/supervisor/scripts/x11vnc.sh
@@ -6,7 +6,6 @@ x11vnc -storepasswd ${PASSWORD} ${HOME}/.vnc/passwd
 chmod 600 ${HOME}/.vnc/passwd
 
 # starts VNC
-# https://stackoverflow.com/questions/18351198/what-are-the-uses-of-the-exec-command-in-shell-scripts
 exec /usr/bin/x11vnc \
   -display ${DISPLAY} \
   -rfbauth ${HOME}/.vnc/passwd \
